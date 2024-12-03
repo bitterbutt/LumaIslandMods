9c23a00df417551aa6e187a00a59534df7227ae0
bitterbutt <114014582+bitterbutt@users.noreply.github.com>
2024-12-03 15:33:09 -0500

    fix: transpiler for lobby controller

--- LargeLobby/Plugin.cs
+++ LargeLobby/Plugin.cs
@@ -1,56 +1,98 @@
-﻿using BepInEx;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
 using TMPro;
 
-namespace LargeLobby;
-
-[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-public class Plugin : BaseUnityPlugin
+namespace LargeLobby
 {
-    internal static new ManualLogSource Logger;
-
-    private void Awake()
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
     {
-        Logger = base.Logger;
-        Harmony.CreateAndPatchAll(typeof(JoinGameRowPatch));
-        Harmony.CreateAndPatchAll(typeof(JoinGameRowButtonPatch));
-        Harmony.CreateAndPatchAll(typeof(FriendInfoPatch));
-        Logger.LogWarning("LargeLobby is now enabled.");
-    }
+        internal static ManualLogSource Log;
 
-    [HarmonyPatch(typeof(JoinGameRow), "OnJoinGame")]
-    public class JoinGameRowPatch
-    {
-        private static bool Prefix(JoinGameRow __instance)
+        private void Awake()
         {
-            FriendLobbyInfo value = Traverse.Create(__instance).Field<FriendLobbyInfo>("m_lobby").Value;
-            if (value != null)
+            Log = base.Logger;
+            Harmony.CreateAndPatchAll(typeof(JoinGameRowPatch));
+            Harmony.CreateAndPatchAll(typeof(JoinGameRowButtonPatch));
+            Harmony.CreateAndPatchAll(typeof(FriendInfoPatch));
+            Harmony.CreateAndPatchAll(typeof(SteamLobbyControllerPatch));
+            Log.LogWarning("LargeLobby is now enabled.");
+        }
+
+        [HarmonyPatch(typeof(JoinGameRow), "OnJoinGame")]
+        public class JoinGameRowPatch
+        {
+            private static readonly FieldInfo m_lobby_Field = AccessTools.Field(typeof(JoinGameRow), "m_lobby");
+
+            private static bool Prefix(JoinGameRow __instance)
             {
-                value.JoinLobby();
+                FriendLobbyInfo value = (FriendLobbyInfo)m_lobby_Field.GetValue(__instance);
+                if (value != null)
+                {
+                    value.JoinLobby();
+                }
+                return false;
             }
-            return false;
         }
-    }
 
-    [HarmonyPatch(typeof(JoinGameRow), "Initialize")]
-    public class JoinGameRowButtonPatch
-    {
-        public static void Postfix(JoinGameRow __instance, FriendLobbyInfo lobbyInfo)
+        [HarmonyPatch(typeof(JoinGameRow), "Initialize")]
+        public class JoinGameRowButtonPatch
         {
-            TMP_Text value = Traverse.Create(__instance).Field<TMP_Text>("m_playersInGameText").Value;
-            ButtonWidget value2 = Traverse.Create(__instance).Field<ButtonWidget>("m_joinGameButton").Value;
-            value.text = lobbyInfo.PlayersInGame.ToString() + " Players";
-            value2.Interactable = true;
+            private static readonly FieldInfo m_playersInGameText_Field = AccessTools.Field(typeof(JoinGameRow), "m_playersInGameText");
+            private static readonly FieldInfo m_joinGameButton_Field = AccessTools.Field(typeof(JoinGameRow), "m_joinGameButton");
+
+            public static void Postfix(JoinGameRow __instance, FriendLobbyInfo lobbyInfo)
+            {
+                TMP_Text value = (TMP_Text)m_playersInGameText_Field.GetValue(__instance);
+                ButtonWidget value2 = (ButtonWidget)m_joinGameButton_Field.GetValue(__instance);
+                value.text = $"{lobbyInfo.PlayersInGame} Players";
+                value2.Interactable = true;
+            }
         }
-    }
 
-    [HarmonyPatch(typeof(FriendInfo), "CanInvite")]
-    public class FriendInfoPatch
-    {
-        private static void Postfix(ref bool __result, FriendInfo __instance)
+        [HarmonyPatch(typeof(FriendInfo), "CanInvite")]
+        public class FriendInfoPatch
         {
-            __result = !__instance.IsInGameWithHost;
+            private static void Postfix(ref bool __result, FriendInfo __instance)
+            {
+                __result = !__instance.IsInGameWithHost;
+            }
+        }
+
+        [HarmonyPatch(typeof(SteamLobbyController), "OnReceiveLobbyData")]
+        class SteamLobbyControllerPatch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                bool patched = false;
+                for (int i = 0; i <= codes.Count - 6; i++)
+                {
+                    if (codes[i].opcode == OpCodes.Ldloc_S && codes[i + 1].opcode == OpCodes.Ldc_I4_4 &&
+                        codes[i + 2].opcode == OpCodes.Clt && codes[i + 3].opcode == OpCodes.Ldc_I4_0 &&
+                        codes[i + 4].opcode == OpCodes.Ceq && codes[i + 5].opcode == OpCodes.Stloc_S)
+                    {
+                        var flag2Local = codes[i + 5].operand;
+                        codes.RemoveRange(i, 6);
+                        codes.Insert(i, new CodeInstruction(OpCodes.Ldc_I4_0));
+                        codes.Insert(i + 1, new CodeInstruction(OpCodes.Stloc_S, flag2Local));
+                        patched = true;
+                        break;
+                    }
+                }
+
+                if (!patched)
+                {
+                    Log.LogError("Failed to patch SteamLobbyController.OnReceiveLobbyData.");
+                }
+
+                return codes.AsEnumerable();
+            }
         }
     }
 }
